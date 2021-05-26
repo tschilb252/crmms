325ec7f5fb181ce93af9f0379f08431adbecbdbc
Pivarnik <apivarnik@usbr.gov>
2021-05-26 11:17:42 -0700

    renamed 1.SetMtomDirectory.bat file to 1.SetCrmmsDirectory
    edited the bat file to set the environment variable as CRMMS_DIR

--- 1.SetCrmmsDirectory.bat
+++ 1.SetCrmmsDirectory.bat
@@ -1,18 +1,18 @@
-:: This batch file is meant to be used to run MTOM in batch mode using preconfigured input files 
+:: This batch file is meant to be used to run CRMMS in batch mode using preconfigured input files 
 ::		and an initialized RiverWare model
-:: Set MTOM environment variable MTOM_DIR to current directory
+:: Set CRMMS environment variable CRMMS_DIR to current directory
 
 @echo off
 echo ------------------------------------------------
-echo This script sets MTOM_DIR to the directory that 
+echo This script sets CRMMS_DIR to the directory that 
 echo   currently contains this script file. 
 echo ------------------------------------------------
 echo.
-echo Setting local MTOM_DIR environment variable to:
+echo Setting local CRMMS_DIR environment variable to:
 echo %cd%
 echo.
 echo ------------------------------------------------
-setx MTOM_DIR "%cd%
+setx CRMMS_DIR "%cd%
 echo.
 echo ------------------------------------------------
 pause
