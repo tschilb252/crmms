630fd0c9aadfcc94991c17650dba6a5cb24bb5e5
stighi <stighi@usbr.gov>
2024-02-16 14:00:32 -0800

    Feb 2024 CRMMS-ESP files

--- Code/5-YrScripts/0_MasterInputs.R
+++ Code/5-YrScripts/0_MasterInputs.R
@@ -7,11 +7,11 @@ rm(list=ls())
 ## -- Inputs
 # Scenarios: "Name of Scenario" = "Directory name" 
 #   (data are in sub-directories of the Scenario folder)
-scenario_dir <- c("Jan" = "2024-01_ESP",
-                  "Dec" = "2023-12_ESP")
+scenario_dir <- c("Feb" = "2024-02_ESP",
+                  "Jan" = "2024-01_ESP")
 
 
-fig_dir_nm <- '2024-01_2yr_Compare'
+fig_dir_nm <- '2024-02_2yr_Compare'
 # ^ script will create directory in Results/ with this name if it doesn't exist
 
 ## Scenario colors
@@ -27,7 +27,7 @@ source(file.path('Code', '5-YrScripts','LB_ICSUse.R'))
 source(file.path('Code', '5-YrScripts','Compare_Slots_Cloud+Trace.R'))
 source(file.path('Code', '5-YrScripts','PowellTiers_LBCondition.R'))
 source(file.path('Code', '5-YrScripts','PowellMead_Thresholds.R'))
-source(file.path('Code', '5-YrScripts','PowellTiers_RelPE_summ.R'))
+source(file.path('Code', '5-YrScripts','PowellMead_RelPE_summ.R'))
 source(file.path('Code', '5-YrScripts','5yr_table.R'))
 source(file.path('Code', '5-YrScripts','Trace_Data.R'))
 source(file.path('Code', '5-YrScripts','PowellUnreg_vs.tarv,etc.R'))
